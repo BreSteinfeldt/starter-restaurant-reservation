367ab4013ec42cd60710374114bc56ac651ab4bd
Breanna Steinfeldt <breanna.steinfeldt@gmail.com>
2024-01-22 13:46:53 -0500

    worked through US05 & US06 features for frontend

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -101,3 +101,27 @@ export async function seatTable(reservation_id, table_id, signal) {
   };
   return await fetchJson(url, options);
 }
+
+export async function updateReservation(reservation, newStatus, signal) {
+  const url = `${API_BASE_URL}/reservations/${reservation.reservation_id}/status`;
+  const options = {
+    method: `PUT`,
+    headers,
+    body: JSON.stringify({ data: { status: newStatus } }),
+    signal,
+  };
+  return await fetchJson(url, options);
+}
+
+export async function unseatTable(table_id, signal) {
+  const url = `${API_BASE_URL}/tables/${table_id}/seat`;
+  const options = {
+    method: `DELETE`,
+    headers,
+    body: JSON.stringify({
+      data: {},
+    }),
+    signal,
+  };
+  return await fetchJson(url, options);
+}
