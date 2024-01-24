33db58bdb074fc028d8e790e1218106eb43d516e
Breanna Steinfeldt <breanna.steinfeldt@gmail.com>
2024-01-24 11:37:38 -0500

    added the edit feature for US08 in the backend

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -4,27 +4,33 @@ function list(date) {
   return knex("reservations")
     .select("*")
     .where({ reservation_date: date })
-    .orderBy("reservation_time")
+    .whereNot({ status: "finished" })
+    .orderBy("reservation_time");
 }
 
-
-function create(reservation){
-    return knex("reservations")
-     .insert(reservation)
-     .returning("*")
-     .then((saved) => saved[0])
-}
-
-function read(reservation_id) {
+function create(reservation) {
   return knex("reservations")
-   .where({ reservation_id: reservation_id })
-   .first();
+    .insert(reservation)
+    .returning("*")
+    .then((saved) => saved[0]);
+}
+
+function read(resId) {
+  return knex("reservations")
+   .where({ reservation_id: resId });
 }
 
 function update(reservation) {
   return knex("reservations")
     .where({ reservation_id: reservation.reservation_id })
     .update(reservation, "*")
+    .then((updated) => updated[0]);
+}
+
+function updateStatus(reservation_id, status) {
+  return knex("reservations")
+    .where({ reservation_id: reservation_id })
+    .update({ status: status }, "*")
     .then((updated) => updated[0]);
 }
 
@@ -42,5 +48,6 @@ module.exports = {
   create,
   read,
   update,
+  updateStatus,
   search,
 };
